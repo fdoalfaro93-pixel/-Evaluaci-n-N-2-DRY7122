28a8d301df340bd22b4e380ceb20f9ebaf7258c4
Fernando Alfaro <fdo.alfaro93@gmail.com>
2026-05-22 00:19:30 -0400

    Docker y CI/CD

--- sample-app.sh
+++ sample-app.sh
@@ -10,7 +10,8 @@ cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
 echo "FROM python:3.9" >> tempdir/Dockerfile
-echo "RUN pip install --no-input --no-cache-dir flask" >> tempdir/Dockerfile
+echo "ENV PIP_PROGRESS_BAR=off" >> tempdir/Dockerfile
+echo "RUN pip install --progress-bar off flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
@@ -18,7 +19,7 @@ echo "EXPOSE 9999" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
-docker build --security-opt seccomp=unconfined -t sampleapp .
+docker build -t sampleapp .
 docker rm -f samplerunning 2>/dev/null
 docker run -t -d -p 9999:9999 --name samplerunning sampleapp
 docker ps -a
