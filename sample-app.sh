32c6a717a27d1a0020a7b492605168bbad19219b
Fernando Alfaro <fdo.alfaro93@gmail.com>
2026-05-22 00:01:47 -0400

    Docker y CI/CD

--- sample-app.sh
+++ sample-app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf tempdir
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
@@ -8,8 +9,8 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "FROM python:3.9" >> tempdir/Dockerfile
+echo "RUN pip install --no-input flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
@@ -18,5 +19,7 @@ echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
+docker rm -f samplerunning 2>/dev/null
 docker run -t -d -p 9999:9999 --name samplerunning sampleapp
-docker ps -a 
+docker ps -a
+
