61fd6a8e7d9fcde0792ca29730dcecb6a252df77
Fernando Alfaro <fdo.alfaro93@gmail.com>
2026-05-22 00:30:18 -0400

    Docker y CI/CD

--- sample-app.sh
+++ sample-app.sh
@@ -9,20 +9,19 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-cat > tempdir/Dockerfile << 'DOCKERFILE'
-FROM python:3.9
-ENV PIP_PROGRESS_BAR=off
-RUN pip install --progress-bar off flask
-COPY ./static /home/myapp/static/
-COPY ./templates /home/myapp/templates/
-COPY sample_app.py /home/myapp/
-WORKDIR /home/myapp
-EXPOSE 9999
-CMD ["python", "sample_app.py"]
-DOCKERFILE
+echo "FROM python:3.9" > tempdir/Dockerfile
+echo "ENV PIP_PROGRESS_BAR=off" >> tempdir/Dockerfile
+echo "RUN pip install --progress-bar off flask" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "WORKDIR /home/myapp" >> tempdir/Dockerfile
+echo "EXPOSE 9999" >> tempdir/Dockerfile
+printf 'CMD ["python", "sample_app.py"]\n' >> tempdir/Dockerfile
 
 cd tempdir
-docker build -t sampleapp .
+docker rmi -f sampleapp 2>/dev/null
+docker build --no-cache -t sampleapp .
 docker rm -f samplerunning 2>/dev/null
 docker run -d -p 9999:9999 --name samplerunning sampleapp
 docker ps -a
